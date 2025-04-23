a69dc54291e78de0a294d630829d1353cb073d4a
Lucas Bubner <lkbubner@proton.me>
2025-04-23 21:14:52 +0930

    Update config.js

--- StatsRing/config.js
+++ StatsRing/config.js
@@ -1,6 +1,6 @@
-import { @Vigilant, @SwitchProperty } from "../Vigilance/index.js";
+import { @Vigilant, @SwitchProperty, @TextProperty } from "../Vigilance/index.js";
 
-@Vigilant("StatsRing", "§lStatsRing Health and Mana ring")
+@Vigilant("StatsRing", "§lStats Ring: §r§cHealth §rand §bMana §rring")
 
 class Settings {
     constructor() {
@@ -8,18 +8,60 @@ class Settings {
     }
 
     @SwitchProperty({
-        name: "Toggle",
-        description: "Toggle ring on/off",
-        category: "Settings"
+        name: "Enable ring",
+        description: "Render the ring (on/off).",
+        category: "Core"
     })
     active = true;
 
     @SwitchProperty({
-        name: "Percentages",
-        description: "Render percentages of health/mana next to ring",
-        category: "Settings"
+        name: "Show percentages",
+        description: "Render percentages of health/mana next to ring (on/off).",
+        category: "Core"
     })
-    percentage = false;
+    percentage = true;
+
+    @SwitchProperty({
+        name: "Show absorption",
+        description: "Show over 100%% health for absorption hearts (on/off).",
+        category: "Core"
+    })
+    absorption = true;
+
+    @SwitchProperty({
+        name: "Show backing image",
+        description: "Show the background image for the ring that surround the bars (on/off).",
+        category: "Display"
+    })
+    backingImage = true;
+
+    @SwitchProperty({
+        name: "Interpolate colour",
+        description: "Linear interpolates mana and health colour depending on percentage filled (on/off).",
+        category: "Display"
+    })
+    interpolateColour = true;
+
+    @SwitchProperty({
+        name: "Interpolate bars",
+        description: "Linear interpolates the filled progress of the bars (on/off).",
+        category: "Display"
+    })
+    interpolateBars = true;
+
+    @TextProperty({
+        name: "Low HP percent alert threshold",
+        description: "Visually flashes HP when equal to or below this percentage (%% from 0 to 100, -1 to disable)",
+        category: "Visual Warnings",
+    })
+    alertLowHpPercent = "40";
+
+    @TextProperty({
+        name: "Low Mana percent alert threshold",
+        description: "Visually flashes Mana when equal to or below this percentage (%% from 0 to 100, -1 to disable)",
+        category: "Visual Warnings",
+    })
+    alertLowManaPercent = "20";
 }
 
 export default new Settings;
